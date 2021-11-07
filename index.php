590eeb5684ba2477143bf4835904d5e8c45c92c8
Thanh Huy <huynguyen.200700@gmail.com>
2021-11-07 20:45:22 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Zay Shop eCommerce HTML CSS Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/templatemo.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+<!--
+    
+TemplateMo 559 Zay Shop
+
+https://templatemo.com/tm-559-zay-shop
+
+-->
+</head>
+
+<body>
+    <!-- Start Top Nav -->
+    <?php include'./pages/nav.php'?>
+    <!-- Close Top Nav -->
+
+
+    <!-- Header -->
+    <?php include"./pages/header.php"?>
+    <!-- Close Header -->
+
+    <!-- Modal -->
+    <?php include"./pages/modal.php" ?>
+
+<!-- end modal -->
+    <!-- Start Banner Hero -->
+    <?php include"./pages/banner.php" ?>
+    <!-- End Banner Hero -->
+
+
+    <!-- Start Categories of The Month -->
+    <?php include"./pages/categories.php" ?>
+    <!-- End Categories of The Month -->
+
+
+    <!-- Start Featured Product -->
+   <?php include"./pages/product.php" ?>
+    <!-- End Featured Product -->
+
+
+    <!-- Start Footer -->
+    <?php include"./pages/footer.php" ?>
+    <!-- End Footer -->
+
+    <!-- Start Script -->
+    <script src="assets/js/jquery-1.11.0.min.js"></script>
+    <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/templatemo.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <!-- End Script -->
+</body>
+
+</html>
